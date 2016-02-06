b3f260d7d84b42319e3f5f0f64fc4810950c4410
Student <s521566@nw35016.nwmissouri.edu>
2016-02-06 16:40:20 -0600

    initial calculator

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -9,17 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isTypingNumber = false
+    var firstNumber = 0
+    var secondNumber = 0
+    var answer = 0
+    var operation:String = ""
+    
+    
+    
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    @IBAction func numberTapped(sender: AnyObject) {
+        let number = sender.currentTitle!
+        if isTypingNumber {
+            
+            if let input = calculatorDisplay.text {
+                    calculatorDisplay.text = (input + number!)
+            }
 
+        }
+        else {
+            isTypingNumber = true
+            calculatorDisplay.text = number
+            
+        }
+        
+        
+    }
+    
+    @IBAction func calculationTappedNegetive(sender: AnyObject) {
+        firstNumber = Int(calculatorDisplay.text!)!
+        calculatorDisplay.text = nil
+        operation =  sender.currentTitle!!
+        isTypingNumber=false
+        
+        
+        
+        
+    }
+    @IBAction func calculationTappedPositive(sender: AnyObject) {
+        firstNumber = Int(calculatorDisplay.text!)!
+        calculatorDisplay.text = nil
+        operation =  sender.currentTitle!!
+    }
+    @IBAction func equalsTapped(sender: AnyObject) {
+        secondNumber = Int(calculatorDisplay.text!)!
+        
+        switch operation {
+            
+        case "+":
+            answer = firstNumber + secondNumber
+            calculatorDisplay.text = answer.description
+            break
+        case "-":
+            answer = firstNumber - secondNumber
+            calculatorDisplay.text = answer.description
+            break
+            
+        default:
+            print("invalid operation")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
